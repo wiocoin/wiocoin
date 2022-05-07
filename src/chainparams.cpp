c992dcf0a4c57e8f6cc5aa0606ef7a17f22880ec
Brothereum <84236781+brothereum@users.noreply.github.com>
2022-05-07 23:17:32 +0530

    Initial commit

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -119,7 +119,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        //vSeeds.emplace_back("seed-a.wiocoin.loshan.co.uk");
+        vSeeds.emplace_back("seed.wio.brochain.org");
         //vSeeds.emplace_back("dnsseed.thrasher.io");
         //vSeeds.emplace_back("dnsseed.wiocointools.com");
         //vSeeds.emplace_back("dnsseed.wiocoinpool.org");
